10753eb913eeda1d9d6ac84aec26aa6e94babe81
bgfxc4 <bindermarc@gmx.de>
2020-07-19 10:39:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ wss.on('connection', function connection(ws) {
                     indexOfUser = i;
                     console.log("logged in user " + users[i]);
                     sessIDs[i] = generateSessionID();
-                    sendMessage(ws, "loggedIn:" + encryptAes(passwords[indexOfUser],getSessionID(indexOfUser)) + " " +   fs.readFile('demofile1.html', function(err, data) {return data}));
+                    sendMessage(ws, "loggedIn:" + encryptAes(passwords[indexOfUser],getSessionID(indexOfUser)) + " " +   fs.readFile('./htmls/adminArea.html', function(err, data) {return data}));
                     console.log(sessIDs);
                     return;
                 }
